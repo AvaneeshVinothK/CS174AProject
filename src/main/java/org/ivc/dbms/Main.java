bcb2b171de09b266c4d3943299f67a96d20671f2
AvaneeshVinoth <78749983+AvaneeshVinoth@users.noreply.github.com>
2026-05-19 14:17:28 -0700

    Refactored main

--- src/main/java/org/ivc/dbms/Main.java
+++ src/main/java/org/ivc/dbms/Main.java
@@ -2,51 +2,65 @@ package org.ivc.dbms;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Scanner;
+import java.io.FileInputStream;
 
 public class Main {
-    public static Connection conn;
+    public static Connection emartConn;
+    public static Connection depotConn;
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        String walletPath = "C:/Users/Avaneesh/OneDrive/UCSB_Classes/Spring2026/CS174A/Project/Wallet_CS174AProject";
-        
-        Properties props = new Properties();
-        props.setProperty("user", "ADMIN");
-        props.setProperty("password", "3e6vF#5qw#9DP_$");
-        props.setProperty("oracle.net.wallet_location",
-            "(SOURCE=(METHOD=FILE)(METHOD_DATA=(DIRECTORY=" + walletPath + ")))");
-        props.setProperty("oracle.net.tns_admin", walletPath);
+        Properties config = new Properties();
+        config.load(new FileInputStream("config.properties"));
 
-        String url = "jdbc:oracle:thin:@cs174aproject_tp?TNS_ADMIN=" + walletPath;
-        conn = DriverManager.getConnection(url, props);
-        conn.setAutoCommit(false);
-        System.out.println("Connected to database.");
+        // eMART connection
+        String emartWallet = config.getProperty("emart.wallet");
+        Properties emartProps = new Properties();
+        emartProps.setProperty("user", "ADMIN");
+        emartProps.setProperty("password", config.getProperty("emart.password"));
+        emartProps.setProperty("oracle.net.wallet_location",
+            "(SOURCE=(METHOD=FILE)(METHOD_DATA=(DIRECTORY=" + emartWallet + ")))");
+        emartProps.setProperty("oracle.net.tns_admin", emartWallet);
+        emartConn = DriverManager.getConnection(
+            "jdbc:oracle:thin:@" + config.getProperty("emart.tns") + "?TNS_ADMIN=" + emartWallet,
+            emartProps);
+        emartConn.setAutoCommit(false);
+        System.out.println("Connected to eMART database.");
+
+        // eDEPOT connection
+        String depotWallet = config.getProperty("depot.wallet");
+        Properties depotProps = new Properties();
+        depotProps.setProperty("user", "ADMIN");
+        depotProps.setProperty("password", config.getProperty("depot.password"));
+        depotProps.setProperty("oracle.net.wallet_location",
+            "(SOURCE=(METHOD=FILE)(METHOD_DATA=(DIRECTORY=" + depotWallet + ")))");
+        depotProps.setProperty("oracle.net.tns_admin", depotWallet);
+        depotConn = DriverManager.getConnection(
+            "jdbc:oracle:thin:@" + config.getProperty("depot.tns") + "?TNS_ADMIN=" + depotWallet,
+            depotProps);
+        depotConn.setAutoCommit(false);
+        System.out.println("Connected to eDEPOT database.");
 
         while (true) {
             System.out.println("\n=== Welcome to eMART ===");
             System.out.println("1. Customer");
             System.out.println("2. Manager");
-            System.out.println("3. Exit");
+            System.out.println("3. eDEPOT");
+            System.out.println("4. Exit");
             System.out.print("Choose: ");
             int choice = Integer.parseInt(scanner.nextLine());
 
-            if (choice == 1) Customer.customerMenu();
+            if (choice == 1)      Customer.customerMenu();
             else if (choice == 2) Manager.managerMenu();
+            else if (choice == 3) Depot.depotMenu();
             else break;
         }
 
-        conn.close();
+        emartConn.close();
+        depotConn.close();
         System.out.println("Goodbye!");
-    }
-
-    public static String generateCustomerId() throws SQLException {
-        ResultSet rs = conn.createStatement().executeQuery("SELECT COUNT(*) FROM Customer");
-        rs.next();
-        int count = rs.getInt(1);
-        return "CUST" + String.format("%04d", count + 1);
     }
 }
